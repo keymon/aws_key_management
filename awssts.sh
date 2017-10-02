47914131605714f85b705cc7d1925c5d7d55a04b
Hector Rivas Gandara <keymon@gmail.com>
2017-10-02 18:49:02 +0100

    Refactor awssts.sh to split logic and function
    To simplify awssts.sh, we would split the implementation in two
    clear parts:
    
     - The function definition, which would call the script awssts.sh
       and eval the value.
    
     - The script with the logic and the set of configured accounts.
       This part will only be executed when the script is NOT being
       sourced. We detect if the script is being sourced by checking
       `[ "$BASH_SOURCE" != "$0" ]`. See [1] for more info.
    
    This change gets us a step further to split the hardcoded specific
    config from the logic of the function.
    
    We also add:
     - logic to print the configured accounts when no args are passed.
     - fix error handling in the function.
     - Add base users, retrieved directly form pass, and assume roles.
       Prefix them with user: or role:
     - Pass ROOT_AWS_CREDENTIALS_COMMAND to the
       `cached-sts-token.sh` command. It actually would call `awssts.sh`
       itself to get the user:* corresponding account.
    
    [1] https://stackoverflow.com/a/2687092/395686

--- awssts.sh
+++ awssts.sh
@@ -1,34 +1,69 @@
+#!/bin/bash
 _awssts_dir="$(command cd "$(dirname "${BASH_SOURCE}")"; pwd)"
+_awssts_script="${_awssts_dir}/${BASH_SOURCE##*/}"
 awssts() {
-  profile="$1"; shift
+  aws_account_name="$1"; shift
 
   # if an argument is passed, execute it
   if [ "$1" ]; then
-    ( awssts "${profile}"; $@ );  return $?
+    ( set -e; awssts "${aws_account_name}"; $@ ); return $?
   fi
 
-  local _aws_cache_gpg_id=2EA619ED
-  case "$profile" in
-    admin@keytwine-root)
-      eval $(pass keytwine/aws/hector.rivas+aws.admin_credentials.sh)
-      eval $(
-        AWS_CACHE_GPG_ID="${_aws_cache_gpg_id}" \
-        ${_awssts_dir}/cached-sts-token.sh \
-          admin@keytwine-root -r admin -m
-      )
-      ;;
-    dev@keytwine-root)
-      eval $(pass keytwine/aws/hector.rivas+aws.dev_credentials.sh)
-      eval $(
-        AWS_CACHE_GPG_ID="${_aws_cache_gpg_id}" \
-        ${_awssts_dir}/cached-sts-token.sh \
-          dev@keytwine-root -r dev -m
-      )
-      ;;
-    *)
-      echo "awssts <aws_profile>"
-      return 1
-      ;;
-  esac
+  eval $(${_awssts_script} "${aws_account_name}")
 }
 
+# Check if the script is being sourced or not
+# More info https://stackoverflow.com/a/2687092/395686
+if [ "$BASH_SOURCE" != "$0" ]; then
+  # The file is being source, stop processing
+  return 0
+fi
+
+_aws_cache_gpg_id=2EA619ED
+
+aws_account_name="$1"; shift
+case "$aws_account_name" in
+  user:hector.rivas+admin@keytwine)
+    pass keytwine/aws/hector.rivas+aws.admin_credentials.sh
+    ;;
+  user:hector.rivas+dev@keytwine)
+    pass keytwine/aws/hector.rivas+aws.dev_credentials.sh
+    ;;
+  role:admin@keytwine-root)
+    ROOT_AWS_CREDENTIALS_COMMAND="${_awssts_script} user:hector.rivas+admin@keytwine" \
+    AWS_CACHE_GPG_ID="${_aws_cache_gpg_id}" \
+      ${_awssts_dir}/cached-sts-token.sh \
+      ${aws_account_name} -r admin -m
+    ;;
+  role:dev@keytwine-root)
+    ROOT_AWS_CREDENTIALS_COMMAND="${_awssts_script} user:hector.rivas+dev@keytwine" \
+    AWS_CACHE_GPG_ID="${_aws_cache_gpg_id}" \
+      ${_awssts_dir}/cached-sts-token.sh \
+      ${aws_account_name} -r dev -m
+    ;;
+  role:admin@keytwine-sandbox)
+    account_id="$(pass keytwine/aws/sandbox/account_id)"
+    ROOT_AWS_CREDENTIALS_COMMAND="${_awssts_script} user:hector.rivas+admin@keytwine" \
+    AWS_CACHE_GPG_ID="${_aws_cache_gpg_id}" \
+      ${_awssts_dir}/cached-sts-token.sh \
+      ${aws_account_name} \
+      -a "${account_id}" \
+      -r admin -m
+    ;;
+  role:dev@keytwine-sandbox)
+    account_id="$(pass keytwine/aws/sandbox/account_id)"
+    ROOT_AWS_CREDENTIALS_COMMAND="${_awssts_script} user:hector.rivas+dev@keytwine" \
+    AWS_CACHE_GPG_ID="${_aws_cache_gpg_id}" \
+      ${_awssts_dir}/cached-sts-token.sh \
+      ${aws_account_name} -a "${account_id}" -r dev -m
+    ;;
+  *)
+    (
+      echo "Usage: awssts <aws_account_name>"
+      echo
+      echo "Available accounts:"
+      sed -n "s/^ *\(user:.*\))/  awssts \1/p;s/^ *\(role:.*\))/  awssts \1/p" < "${_awssts_script}" 1>&2
+    ) 1>&2
+    exit 1
+    ;;
+esac
