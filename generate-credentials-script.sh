b914bd44fc4c8896c19d3ceecc3d23b731aec5c8
Hector Rivas Gandara <keymon@gmail.com>
2018-11-03 18:26:28 +0000

    Ask for aws keys separately
    That way is easier to parse in the script

--- generate-credentials-script.sh
+++ generate-credentials-script.sh
@@ -8,7 +8,11 @@ having the AWS_SECRET_ACCESS_KEY encrypted with GPG.
 
 EOF
 
-read -sp "Access Key ID and Secret Access Key? " AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
+read -sp "Access Key ID? " AWS_ACCESS_KEY_ID
+export AWS_ACCESS_KEY_ID
+echo 2>&1
+read -sp "Secret Access Key?" AWS_SECRET_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY
 echo 2>&1
 echo 2>&1
 
@@ -18,7 +22,7 @@ encoded_secret_access_key="$(echo "$AWS_SECRET_ACCESS_KEY" | gpg -e ${GPG_ID:+-r
 
 
 cat << EOF
-export AWS_ACCESS_KEY_ID="AKIAI3A7KUQ66Z3MYUPQ"
+export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
 export AWS_SECRET_ACCESS_KEY="\$(echo '${encoded_secret_access_key}' | base64 --decode | gpg -d --no-verbose --quiet --batch )";
 export AWS_SESSION_TOKEN=""
 EOF
